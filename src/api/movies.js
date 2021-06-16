b0de2962da43e23aa61adcbc89629d978d53d5b2
Aulia Sabri <auliasabri43@gmail.com>
2021-06-16 12:01:05 +0700

    update

--- src/api/movies.js
+++ src/api/movies.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export const fetchMovies = async (keyword) => {
   return await axios
-    .get(`https://www.omdbapi.com/?apikey=3fa7021f&s=${keyword}`)
+    .get(`http://www.omdbapi.com/?apikey=3fa7021f&s=${keyword}`)
     .then((response) => response.data.Search);
 };
 
 export const fetchMoviesById = async (id) => {
   return await axios
-    .get(`https://www.omdbapi.com/?apikey=3fa7021f&i=${id}&plot=full`)
+    .get(`http://www.omdbapi.com/?apikey=3fa7021f&i=${id}&plot=full`)
     .then((response) => response.data);
 };
